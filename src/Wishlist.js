8e77edd05ec12f54911251c993dd83f79356af73
Yaswanth963 <perumallayaswanth2000@gmail.com>
2024-10-24 22:26:57 +0530

    Updated wishlist

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import {
   Button,
   Typography,
@@ -9,37 +9,83 @@ import {
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete"; // Import Delete icon
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"; // Import Shopping Cart icon
-import "./custom.css"; // Assuming you have this for custom styling
+import "./custom.css";
+import axios from 'axios';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Wishlist = ({ wishlist, removeFromWishlist, moveToCart }) => {
+const Wishlist = () => {
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    const fetchWishlistItems = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/wishlist`);
+            setWishlist(response.data?.items);
+        } catch (error) {
+            console.error('Error fetching wishlist items:', error);
+        }
+    };
+
+    fetchWishlistItems();
+}, []);
+
+  const removeFromWishlist = async (id) => {
+    try {
+      const response = await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/wishlist/${id}`);
+      setWishlist(response.data);
+      toast.success("Item removed from wishlist.");
+    } catch (error) {
+      toast.error("Failed to remove item from wishlist.");
+    }
+  };
+
+  const moveToCart = async (product) => {
+    await removeFromWishlist(product.productId._id);
+    addToCart(product);
+  };
+
+  const addToCart = async (product) => {
+    const body = {
+      productId: product?.productId._id,
+      quantity: 1
+    }
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/cart`, body);
+      toast.success(`${product.name} added to cart.`);
+    } catch (error) {
+      toast.error("Failed to add item to cart.");
+    }
+  };
+  
   return (
     <div className="wishlist-container">
       <Typography variant="h4" align="center" gutterBottom>
         Wishlist
       </Typography>
       <Grid container spacing={2} padding={5}>
-        {wishlist.length === 0 ? (
+        {wishlist?.length === 0 ? (
           <Typography variant="h6" align="center">
             Your wishlist is empty!
           </Typography>
         ) : (
-          wishlist.map((item) => (
-            <Grid item xs={12} md={4} key={item.id}>
+          wishlist?.map((item) => (
+            <Grid item xs={12} md={4} key={item.productId._id}>
               <Card className="product-card">
                 <CardMedia
                   component="img"
                   height="200"
-                  image={item.image}
-                  alt={item.name}
+                  image={item.productId.image}
+                  alt={item.productId.name}
                 />
                 <CardContent>
                   <Typography variant="h5" className="product-name">
-                    {item.name.length > 20
-                      ? item.name.substring(0, 20) + "..."
-                      : item.name}
+                    {item.productId.name.length > 20
+                      ? item.productId.name.substring(0, 20) + "..."
+                      : item.productId.name}
                   </Typography>
                   <Button
-                    onClick={() => removeFromWishlist(item.id)}
+                    onClick={() => removeFromWishlist(item.productId._id)}
                     variant="outlined"
                     className="action-button"
                     startIcon={<DeleteIcon />}
